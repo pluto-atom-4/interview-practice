a20d79ae3052d2b697c878511482f966f981325b
Akai Kaede <genko2@hotmail.com>
2026-01-26 18:53:42 -0800

    Add test for handling out-of-range values in find_duplicates function

--- drills/tests/test_find_duplicates.py
+++ drills/tests/test_find_duplicates.py
@@ -13,3 +13,9 @@ class TestFindDuplicates:
     ])
     def test_find_duplicates(self, input_list, expected_output):
         assert find_duplicates(input_list) == expected_output
+
+    def test_edge_case_out_of_range(self):
+        """Test handling of out-of-range values gracefully."""
+        input_list = [1, 2, 3, 4, 33, 5, 6, 7, 8, 9, 10]  # 33 is out of range for length 10
+        expected_output = []
+        assert find_duplicates(input_list) == expected_output
