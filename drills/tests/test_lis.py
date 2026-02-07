d2142c0c82bcb5966eb69d11b1a323fb396775b8
Akai Kaede <genko2@hotmail.com>
2026-02-06 19:45:03 -0800

    Add pseudocode for longest increasing subsequence function

--- drills/tests/test_lis.py
+++ drills/tests/test_lis.py
@@ -1,3 +1,34 @@
+"""
+FUNCTION longest_increasing_subsequence(nums):
+    IF nums is empty: RETURN 0
+
+    INITIALIZE tails as an empty list
+
+    FUNCTION bisect_left(arr, target):
+        left = 0, right = length of arr
+        WHILE left < right:
+            mid = (left + right) // 2
+            IF arr[mid] < target:
+                left = mid + 1
+            ELSE:
+                right = mid
+        RETURN left
+
+    FOR each num in nums:
+        // Use binary search to find where num fits in tails
+        idx = bisect_left(tails, num)
+
+        // If num is larger than all elements, extend the subsequence
+        IF idx == length of tails:
+            APPEND num TO tails
+        // Otherwise, replace the existing tail to maintain potential for longer sequences
+        ELSE:
+            tails[idx] = num
+
+    RETURN length of tails
+
+"""
+
 import pytest
 
 from drills.lis import longest_increasing_subsequence
