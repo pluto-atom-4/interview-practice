3664805369feb5346b8592f3d482f3ad350b5db7
Akai Kaede <genko2@hotmail.com>
2026-02-07 18:13:55 -0800

    Add pseudocode for climb_stairs function

--- drills/tests/test_climb_stairs.py
+++ drills/tests/test_climb_stairs.py
@@ -1,3 +1,26 @@
+"""
+FUNCTION climb_stairs(n):
+    IF n <= 0: RETURN 0
+    IF n EQUALS 1: RETURN 1
+    IF n EQUALS 2: RETURN 2
+
+    // Base cases: ways(1) = 1, ways(2) = 2
+    // We only need the last two values to calculate the current one
+    prev_2_steps = 1
+    prev_1_step = 2
+
+    FOR i FROM 3 TO n:
+        // Current ways is the sum of taking 1 step from (n-1)
+        // and taking 2 steps from (n-2)
+        current = prev_1_step + prev_2_steps
+
+        // Slide the window forward
+        prev_2_steps = prev_1_step
+        prev_1_step = current
+
+    RETURN prev_1_step
+"""
+
 import pytest
 
 from drills.climb_stairs import climb_stairs
